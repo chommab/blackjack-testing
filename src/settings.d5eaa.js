9393aeeaffda1f077f79bc029c3b5b69a0c46074
chommab <136581572+chommab@users.noreply.github.com>
2023-07-26 17:07:28 +0800

    Add files via upload

---
+++ src/settings.d5eaa.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/menu.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "30ece",
+        main: "a4c60"
+    }
+};
